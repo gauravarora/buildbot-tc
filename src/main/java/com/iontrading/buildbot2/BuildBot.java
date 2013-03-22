d989be2fc2aef6fc99e5633a45a0480c9e9a747f
aashish jassal <aashish.jassal@gmail.com>
2013-03-22 17:33:20 +0530

    fix compilation

--- src/main/java/com/iontrading/buildbot2/BuildBot.java
+++ src/main/java/com/iontrading/buildbot2/BuildBot.java
@@ -4,15 +4,11 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.jibble.pircbot.Colors;
 import org.jibble.pircbot.PircBot;
-
-import com.sun.syndication.feed.synd.SyndEntry;
 
 /**
  * @author g.arora@iontrading.com
@@ -40,50 +36,10 @@ public class BuildBot extends PircBot {
     private void start() throws Exception {
         setName(NAME);
         setVerbose(true);
+
         connect("192.168.45.44", 6697);
         joinChannel(CHANNEL);
         sendMessage(CHANNEL, Colors.UNDERLINE + "Up & running, will report build status");
-
-        SCHEDULED_EXECUTOR_SERVICE.scheduleWithFixedDelay(new Runnable() {
-
-            public void run() {
-                try {
-                    System.out.println("Getting builds");
-
-                    for (SyndEntry entry : getBuilds()) {
-                        String key = makeKey(entry.getTitle());
-                        System.out.println("Key is " + key);
-                        if (START.get()) {
-                            REPORTED_BUILDS.add(key);
-                        }
-
-                        if (REPORTED_BUILDS.contains(key)) {
-                            System.out.println("Key found " + key);
-                            continue;
-                        }
-                        System.out.println("Seding key " + key);
-                        if (entry.getAuthor().contains("Failed")) {
-                            bot.sendMessage(CHANNEL, Colors.RED + entry.getTitle() + " (" + entry.getLink() + ")");
-                        }
-                        if (entry.getAuthor().contains("Success")) {
-                            bot.sendMessage(CHANNEL, Colors.DARK_GREEN + entry.getTitle() + " (" + entry.getLink()
-                                    + ")");
-                        }
-                        REPORTED_BUILDS.add(key);
-                    }
-                    START.set(false);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-            private String makeKey(String title) {
-                Matcher matcher = BUILDINFO.matcher(title);
-                matcher.matches();
-                matcher.groupCount();
-                return matcher.group(1) + "_" + matcher.group(2);
-            }
-        }, 1, 120, TimeUnit.SECONDS);
     }
 
     public static void main(String[] args) throws Exception {
