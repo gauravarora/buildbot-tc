9d82ec5513469cb12d328c70bca0ee98741bb4ab
aashish jassal <aashish.jassal@gmail.com>
2013-03-25 18:03:02 +0530

    Private message functionality added with UT running

--- src/main/java/com/iontrading/buildbot2/BuildBot.java
+++ src/main/java/com/iontrading/buildbot2/BuildBot.java
@@ -1,6 +1,8 @@
 package com.iontrading.buildbot2;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -29,6 +31,8 @@ public class BuildBot extends PircBot {
 
     private static final AtomicBoolean START = new AtomicBoolean(true);
 
+    private final List<String> messagesSentToIRC = new ArrayList<String>();
+
     public BuildBot() throws Exception {
         // start();
     }
@@ -43,11 +47,15 @@ public class BuildBot extends PircBot {
     }
 
     public void reportFailureStatusToIRC(final String title, final String link) throws Exception {
-        sendMessage(CHANNEL, Colors.RED + populateMessage(title, link, "Failure"));
+        String message = Colors.RED + populateMessage(title, link, "Failure");
+        sendMessage(CHANNEL, message);
+        messagesSentToIRC.add(message);
     }
 
     public void reportSuccessStatusToIRC(final String title, final String link) throws Exception {
-        sendMessage(CHANNEL, Colors.GREEN + populateMessage(title, link, "Success"));
+        String message = Colors.GREEN + populateMessage(title, link, "Success");
+        sendMessage(CHANNEL, message);
+        messagesSentToIRC.add(message);
     }
 
     private String populateMessage(final String title, final String link, final String status) {
@@ -64,6 +72,9 @@ public class BuildBot extends PircBot {
     @Override
     protected void onPrivateMessage(String sender, String login, String hostname, String message) {
         super.onPrivateMessage(sender, login, hostname, message);
+        for (String messageToSend : messagesSentToIRC) {
+            sendMessage(sender, messageToSend);
+        }
 
     }
 
